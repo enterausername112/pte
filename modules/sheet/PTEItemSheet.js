9e1ebbdfdc91fbfa4c4682e21ee02e27d7ce1f8f
titou <r.titou@riseup.net>
2023-07-23 23:00:35 +0000

    rt

--- modules/sheet/PTEItemSheet.js
+++ modules/sheet/PTEItemSheet.js
@@ -3,8 +3,8 @@ export default class PTEItemSheet extends ItemSheet{
 
     static get defaultOptions() {
         return foundry.utils.mergeObject(super.defaultOptions, {
-          classes: ["pte", "sheet", "item",this.item.data.type],
-          template: `pte/templates/sheets/${this.item.data.type}-sheet.html`,
+          classes: ["pte", "sheet", "item",this.item.type],
+          template: `pte/templates/sheets/${this.item.type}-sheet.html`,
           width: 520,
           height: 480,
           tabs: [{navSelector: ".sheet-tabs", contentSelector: ".sheet-body", initial: "description"}],
